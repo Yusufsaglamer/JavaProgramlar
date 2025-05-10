d5e4ef4e1d71f013410c31faf4e281e83921c505
Yusufsaglamer <168481592+Yusufsaglamer@users.noreply.github.com>
2025-05-10 21:39:54 +0300

    asal

---
+++ asal.java
@@ -0,0 +1,28 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class asal {
+    public static void main(String[] args) {
+     int number;
+     int rule1;
+     int rule2;
+
+     for (int i=1;i<=100;i++){
+         rule1=i/2;
+         rule2=i%2;
+
+
+         if ( rule1==1){
+             System.out.println(i);
+
+         } else if (i>1 && rule2==1) {
+             System.out.println(i);
+
+         }else continue;
+
+
+     }
+
+
+
+    }
+}
